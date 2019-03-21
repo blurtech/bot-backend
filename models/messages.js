4107f74947c5a69393ac101e27e8b9227a030785
MacOSO <auroragamesproject@gmail.com>
2019-03-22 02:24:07 +0700

    Добавил модель в роут с сообщениями

---
+++ models/messages.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let messagesSchema = new Schema({
+    user: String,
+    time: Date,
+    message: String
+});
+
+module.exports = mongoose.model('messages', messagesSchema);
--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let router = express.Router();
+let messages = require('../models/messages');
 
 /* Send greetings */
 router.get('/greetings', function(req, res, next) {
