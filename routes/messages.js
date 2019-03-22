1bc01a3bbfc1845fa6746f7c8082beb0922d3b70
Sergey <srg.grch@gmail.com>
2019-03-22 14:31:26 +0700

    minor changes

--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,15 @@
 let express = require('express');
 let router = express.Router();
-let messages = require('../models/messages');
+let message = require('../models/message');
 
 /* Send greetings */
 router.get('/greetings', function(req, res, next) {
     res.json({message: 'Hello!'});
+});
+
+
+router.post('/', function(req, res, next) {
+    res.json({message: req.body.message + ' no, fuck you'});
 });
 
 module.exports = router;
