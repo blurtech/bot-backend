43e0f461d0eab72dbd170b617d4088c32aa80137
MacOSO <auroragamesproject@gmail.com>
2019-03-23 20:07:54 +0700

    Обновил роут

--- routes/messages.js
+++ routes/messages.js
@@ -5,6 +5,6 @@ require('../models/user.js');
 const controller = require('../controllers/messages');
 
 router.get('/greetings', controller.greetings)
-router.post('/', controller.sendMessage)
+router.get('/:message', controller.sendMessage)
 
 module.exports = router;
