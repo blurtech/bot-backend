eda76dfcb39970465297474732b6c033ec3313ee
Puzovoz <puzovoz@gmail.com>
2019-03-24 10:16:56 +0700

    красивше функция, рабочее другая

--- repositories/messages.js
+++ repositories/messages.js
@@ -3,8 +3,8 @@ const Answers = require('../models/answer');
 
 exports.greetings = () => Answers.findOne( {'special': 'greetings'}, {'message': 1, '_id': 0} );
 
-exports.getKeywords = () => Answers.find().distinct( 'question' );
+exports.getKeywords = () => Answers.find({}, 'question' );
 
 exports.getAnswer = (keyword) => Answers.findOne( {'question': keyword}, 'question' );
 
-exports.getMessage = (id) => Answers.findOne({'_id': id}, {'message': 1, '_id': 0})
+exports.getMessage = (id) => Answers.findById( id, {'message': 1, '_id': 0} )
