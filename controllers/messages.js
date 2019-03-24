0c9f74c8e58c8a2d4aeb1e26d8d31bbf41089432
Puzovoz <puzovoz@gmail.com>
2019-03-24 16:26:41 +0700

    отдает ответы теперь

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,6 +1,12 @@
 const repository = require('../repositories/messages');
 const fuzz = require('fuzzball');
 
+const flat = (input) => {
+	return input.reduce((acc, current) => {
+		return acc.concat(current);
+	}, []);
+}
+
 exports.greetings = async (req, res) => {
     const data = await repository.greetings();
     return res.success(data)
@@ -12,13 +18,17 @@ exports.sendMessage = async (req, res) => {
     for (let prop in data) {
         questions.push(data[prop].question);
     }
-    questions = questions.reduce(function(flat, current) {
-        return flat.concat(current);
-    }, []);
+    questions = flat(questions);
     const options = {
     	limit: 1,
-    	cutoff: 50,
+    	cutoff: 70,
     	unsorted: true
     };
-    return res.success(req.body.message.split(' ').map(word => fuzz.extract(word, questions, options)))
+    questions = req.body.message.split(' ').map(word => fuzz.extract(word, questions, options));
+    questions = flat(questions).reduce((acc, current) => {
+    	acc.push(current[0]);
+    	return acc;
+    }, []);
+    const answer = await repository.getAnswer(questions[0]);
+    return res.success(answer)
 };
